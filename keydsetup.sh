aa3ebffb8bca9af8ad1619a82555a16aa5c19afa
mannmjr <mannmjr@gmail.com>
2024-04-10 12:02:43 +0100

    Add files via upload

---
+++ keydsetup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+
+# Install
+
+apt install -y gcc git
+
+git clone https://github.com/rvaiya/keyd
+cd keyd
+make && make install
+
+cat > /etc/keyd/default.conf << EOL
+[ids]
+
+*
+
+[main]
+leftmeta = C-tab
+EOL
+
+systemctl enable keyd && systemctl start keyd
